12a7d4e5080b12322f1f11c0ace929f805636d38
Gaurav Jaiswal <gaurav.rcet@gmail.com>
2023-05-12 15:19:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  // Get or create user on Chat Engine!
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": " bf2b6bb4-5806-40e9-a039-f185d637d456" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send(`server side is working`);
+});
+
+app.listen(3001);
+
+
